dac17ee53c36c47b886fbd75b11e081e857b28d7
Ian Thompson <ianthompson@nicelion.com>
2025-11-18 18:42:15 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ index.ts
+ oneroster-mocker
+ 
+ Created by Ian Thompson on November 18th 2025
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2025 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+*/
+
+console.log("hello world");
